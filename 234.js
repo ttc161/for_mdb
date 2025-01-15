64c64ca64e799327c22fb58a4004d853a8410028
ttc161 <138458729+ttc161@users.noreply.github.com>
2025-01-15 09:47:35 +0300

    Delete 234.js

--- 234.js
+++
@@ -1,9 +0,0 @@
-function inTrash(id) {
-let ntr = 0;
-let lbi = libById(id);
-if (lbi) {
-let name = lbi.title;
-ntr = sql('SELECT COUNT(*) FROM "' + name + '" WHERE removed = 1').asInt();
-} 
-return ntr;
-}
