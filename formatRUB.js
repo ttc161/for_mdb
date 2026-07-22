2ce5249f3a98f0f7d0d720a2266f76d4a779a9ce
ttc161 <138458729+ttc161@users.noreply.github.com>
2026-07-22 07:53:23 +0300

    Update formatRUB.js

--- formatRUB.js
+++ formatRUB.js
@@ -1,6 +1,6 @@
 function formatRUB(num) {
     // Округляем до 2 знаков и разделяем тысячи пробелами
-    var parts = num.toFixed(2).split('.');
+    var parts = num ? num.toFixed(2).split('.') : 0;
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
     return parts.join(",") + " ₽";
 }
